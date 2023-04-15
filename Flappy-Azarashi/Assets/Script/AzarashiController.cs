06da964676a88056c56b4bc74c76ac3f17c04721
makimaki08 <08.kobayashi@gmail.com>
2023-04-15 20:18:54 +0900

    上下移動に合わせ、アニメーションを設定するロジック追加

--- Flappy-Azarashi/Assets/Script/AzarashiController.cs
+++ Flappy-Azarashi/Assets/Script/AzarashiController.cs
@@ -5,14 +5,18 @@ using UnityEngine;
 public class AzarashiController : MonoBehaviour
 {
     Rigidbody2D rb2d;
+    Animator animator;
+    float angle;
 
     public float maxHeight;
     public float flapVelocity;
+    public float relativeVelocityX;
+    public GameObject sprite;
 
     void Awake()
     {
-        // Awake関数により、コンポーネントを取得
-        rb2d = GetComponent<Rigidbody2D>();
+        rb2d = GetComponent<Rigidbody2D>(); // Rigidbody2Dコンポーネントを取得
+        animator = sprite.GetComponent<Animator>(); // Animatorコンポーネント取得
     }
 
     // Update is called once per frame
@@ -24,11 +28,27 @@ public class AzarashiController : MonoBehaviour
             Flap();
         }
 
+        // 角度を反映
+        ApplyAngle();
+
+        // angleが水平以上だったら、アニメーターのflapフラグをtrueにする
+        animator.SetBool("flap", angle >= 0.0f);
     }
 
     public void Flap()
     {
         // Velocityを直接書き換えて、上方向に加速
         rb2d.velocity = new Vector2(0.0f, flapVelocity);
+    }
+    void ApplyAngle()
+    {
+        // 現在の速度、相対速度から進んでいる角度を求める
+        float targetAngle = Mathf.Atan2(rb2d.velocity.y, relativeVelocityX) * Mathf.Rad2Deg;
+
+        // 回転アニメをスムージング
+        angle = Mathf.Lerp(angle, targetAngle, Time.deltaTime * 10.0f);
+
+        // Rotationの反映
+        sprite.transform.localRotation = Quaternion.Euler(0.0f, 0.0f, angle);
     }
 }
